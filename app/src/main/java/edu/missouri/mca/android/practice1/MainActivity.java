0c7dbe974404517b5cc5f302d0879b8473cb3007
Samuel Holland <samuel@sholland.org>
2018-02-09 19:01:17 -0600

    MainActivity: Make clicking the button show a message

--- app/src/main/java/edu/missouri/mca/android/practice1/MainActivity.java
+++ app/src/main/java/edu/missouri/mca/android/practice1/MainActivity.java
@@ -1,12 +1,30 @@
 package edu.missouri.mca.android.practice1;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import edu.missouri.mca.android.practice1.databinding.MainActivityBinding;
 
 public class MainActivity extends Activity {
+    private MainActivityBinding binding;
+
+    public void onButtonPressed(final View view) {
+        final String emailAddress = binding.textEmail.getText().toString();
+        Toast.makeText(this, getString(R.string.message, emailAddress), Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+    }
+
+    @Override
+    protected void onDestroy() {
+        binding = null;
+        super.onDestroy();
     }
 }
