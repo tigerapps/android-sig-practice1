00fa20128a561ac1563e68b5e4412fa9ab5a9bf8
Samuel Holland <samuel@sholland.org>
2018-02-16 22:08:44 -0600

    Code from week 3

--- app/src/main/java/edu/missouri/mca/android/practice1/MainActivity.java
+++ app/src/main/java/edu/missouri/mca/android/practice1/MainActivity.java
@@ -4,22 +4,23 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import edu.missouri.mca.android.practice1.databinding.MainActivityBinding;
 
 public class MainActivity extends Activity {
+    private final Model activityModel = new Model();
     private MainActivityBinding binding;
 
     public void onButtonPressed(final View view) {
-        final String emailAddress = binding.textEmail.getText().toString();
-        Toast.makeText(this, getString(R.string.message, emailAddress), Toast.LENGTH_SHORT).show();
+        binding.heading.setText(activityModel.getEmail());
     }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        binding.setActivity(this);
+        binding.setModel(activityModel);
     }
 
     @Override
